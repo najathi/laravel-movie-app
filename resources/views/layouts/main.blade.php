f91eb992c32b88f4802d84821765cf7f3bbbb715
dmrfrht <dmrfrht1@gmail.com>
2021-01-07 12:47:55 +0300

    search dropdown with livewire

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,7 @@
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="/css/main.css">
+  <livewire:styles/>
   <title>Movie App</title>
 </head>
 <body class="font-sans bg-gray-900 text-white">
@@ -33,18 +34,11 @@
     </ul>
 
     <div class="flex flex-col md:flex-row items-center">
-      <div class="relative mt-3 md:mt-0">
-        <input
-          type="text"
-          class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline text-sm"
-          placeholder="Search">
-        <div class="absolute top-0">
-          <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"></path></svg>
-        </div>
-      </div>
+      <livewire:search-dropdown />
       <div class="md:ml-4 mt-3 md:mt-0">
         <a href="#">
-          <img src="https://pbs.twimg.com/profile_images/1052707925037379585/LzDX7o9L_400x400.jpg" alt="avatar" class="rounded-full w-8 h-8">
+          <img src="https://pbs.twimg.com/profile_images/1052707925037379585/LzDX7o9L_400x400.jpg" alt="avatar"
+               class="rounded-full w-8 h-8">
         </a>
       </div>
     </div>
@@ -52,5 +46,7 @@
 </nav>
 
 @yield('content')
+
+<livewire:scripts/>
 </body>
 </html>
