81236cabe6cca1bd4203d006489e2b8416f200f0
dmrfrht <dmrfrht1@gmail.com>
2021-01-11 15:30:55 +0300

    adding tv shows

--- resources/views/actors/show.blade.php
+++ resources/views/actors/show.blade.php
@@ -65,13 +65,13 @@
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-8">
           @foreach($knownForMovies as $movie)
             <div class="mt-4">
-              <a href="{{ route('movies.show', $movie['id']) }}">
+              <a href="{{ $movie['linkToPage'] }}">
                 <img
                   src="{{ $movie['poster_path'] }}"
                   alt="poster"
                   class="hover:opacity-75 transition ease-in-out duration-150"
                 >
-                <a href="{{ route('movies.show', $movie['id']) }}"
+                <a href="{{ $movie['linkToPage'] }}"
                    class="text-sm leading-normal block text-gra-400 hover:text-white mt-1">
                   {{ $movie['title'] }}
                 </a>
